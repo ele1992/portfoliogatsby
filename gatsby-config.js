69f930f9daf46582b4f6c4934503500a1582ebd6
ele1992 <tjin.yip@gmail.com>
2021-06-06 19:13:45 +0200

    created prototype for portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: "YipAuYeung",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-notion-api`,
+      options: {
+        token: `secret_TpxWC9S2Vojs5L7ASSlE98IZburD8hs5OIVCV9unbFQ`,
+        databaseId: `75f82346-6e3f-42a3-a2b1-163f59098164`,
+      },
+    },
+    "gatsby-plugin-postcss",
+  ],
 };
