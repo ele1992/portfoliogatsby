71d7f94ee00ba9148b266d9b536c77806f9ee47b
ele1992 <tjin.yip@gmail.com>
2021-06-08 10:48:04 +0200

    removed dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "YipAuYeung",
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-notion-api`,
-      options: {
-        token: `secret_TpxWC9S2Vojs5L7ASSlE98IZburD8hs5OIVCV9unbFQ`,
-        databaseId: `75f82346-6e3f-42a3-a2b1-163f59098164`,
-      },
-    },
-    "gatsby-plugin-postcss",
-  ],
+  plugins: ["gatsby-plugin-postcss"],
 };
